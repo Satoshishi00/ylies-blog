8b555898f083b8620bf270ce019347a3c6603a80
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-06-15 09:39:18 +0200

    utilisation du protocole ssl pour l'envoie de mail

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root',
 })
 export class HttpService {
-  private url = 'http://mailhub.ylies.xyz?message=';
+  private url = 'https://mailhub.ylies.xyz?message=';
 
   private httpOptions = {};
 
