8e188844a99e0c67c1938c3dfa8006bf5cac9635
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-06-15 14:16:29 +0200

    mise en place du service seo (v)

---
+++ src/app/seo.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeoService {
+  constructor(private title: Title, private meta: Meta) {}
+
+  updateTitle(title: string) {
+    this.title.setTitle(title);
+  }
+
+  updateDescription(desc: string) {
+    this.meta.updateTag({ name: 'description', content: desc });
+  }
+
+  updateKeywords(keywords: string) {
+    this.meta.updateTag({ name: 'keywords', content: keywords });
+  }
+
+  updateOgUrl(url: string) {
+    this.meta.updateTag({ property: 'og:url', content: url });
+  }
+
+  updateOgImage(ogimage: string) {
+    this.meta.updateTag({ property: 'og:image', content: ogimage });
+  }
+
+  updateOgTitle(ogtitle: string) {
+    this.meta.updateTag({ property: 'og:title', content: ogtitle });
+  }
+
+  updateOgDesc(ogdesc: string) {
+    this.meta.updateTag({ property: 'og:description', content: ogdesc });
+  }
+}
