aee1ef737d03a402e783645a052d35e3b24cf584
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-06-21 17:15:22 +0200

    component convertisseur salaire brut en net en fonction de poste

--- src/app/brut-net/brut-net.component.ts
+++ src/app/brut-net/brut-net.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brut-net.component.sass'],
 })
 export class BrutNetComponent implements OnInit {
+  field_input_name = '';
+  field_input_amount = 0;
+  contrat_rate = 0;
+
   constructor() {}
 
   ngOnInit(): void {
@@ -20,13 +24,62 @@ export class BrutNetComponent implements OnInit {
 
     for (let field of fields) {
       document.getElementById(field).addEventListener('keyup', (e) => {
-        let amount = e.target['value'];
-        this.calculOtherFields(field, amount);
+        this.field_input_name = field;
+        this.field_input_amount = e.target['value'];
+        this.contrat_rate = this.determineContratName();
+        this.calculOtherFields(
+          field,
+          this.field_input_amount,
+          this.contrat_rate
+        );
+      });
+    }
+
+    this.listenerContratChangement();
+  }
+
+  determineContratName() {
+    let contrats = {
+      'salarie-non-cadre': 0.22,
+      'salarie-cadre': 0.25,
+      'fonction-publique': 0.15,
+      liberale: 0.45,
+      portage: 0.51,
+    };
+
+    let contratRate = 0;
+    for (let contratName in contrats) {
+      contratRate = document.getElementById(contratName)['checked']
+        ? contrats[contratName]
+        : contratRate;
+    }
+
+    return contratRate;
+  }
+
+  listenerContratChangement() {
+    let contrats = {
+      'salarie-non-cadre': 0.22,
+      'salarie-cadre': 0.25,
+      'fonction-publique': 0.15,
+      liberale: 0.45,
+      portage: 0.51,
+    };
+
+    for (let contrat in contrats) {
+      document.getElementById(contrat).addEventListener('click', (e) => {
+        this.contrat_rate = contrats[contrat];
+
+        this.calculOtherFields(
+          this.field_input_name,
+          this.field_input_amount,
+          this.contrat_rate
+        );
       });
     }
   }
 
-  calculOtherFields(name, amount) {
+  calculOtherFields(name, amount, contratRate) {
     //Détermination du montant annuel brut
     let annuel_brut = 0;
     switch (name) {
@@ -34,19 +87,19 @@ export class BrutNetComponent implements OnInit {
         annuel_brut = amount * 151.67 * 12;
         break;
       case 'horaire-net':
-        annuel_brut = amount * 1.2987013 * 151.67 * 12;
+        annuel_brut = ((amount * 1) / (1 - contratRate)) * 151.67 * 12;
         break;
       case 'mensuel-brut':
         annuel_brut = amount * 12;
         break;
       case 'mensuel-net':
-        annuel_brut = amount * 12 * 1.2987013;
+        annuel_brut = (amount * 12 * 1) / (1 - contratRate);
         break;
       case 'annuel-brut':
         annuel_brut = amount;
         break;
       case 'annuel-net':
-        annuel_brut = amount * 1.2987013;
+        annuel_brut = (amount * 1) / (1 - contratRate);
         break;
       default:
         console.log('error : input calculOtherFiels');
@@ -55,7 +108,7 @@ export class BrutNetComponent implements OnInit {
     console.log('calculOtherField', annuel_brut);
 
     //Détermination de tous les autres montant à partir de l'anneul brut
-    let annuel_net = 0.77 * annuel_brut;
+    let annuel_net = (1 - contratRate) * annuel_brut;
     let mensuel_brut = annuel_brut / 12;
     let mensuel_net = annuel_net / 12;
     let horaire_brut = mensuel_brut / 151.67;
