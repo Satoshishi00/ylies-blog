17ffd6069750f4bb66c79955821ef2c7dde9d1bb
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-06-22 14:08:46 +0200

    impots : affichage dynamique de la séparation des milliers dans le champ de saisie

--- src/app/impots/impots.component.ts
+++ src/app/impots/impots.component.ts
@@ -8,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ImpotsComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    document.getElementById('amount').addEventListener('keyup', (e) => {
+      console.log(e.target['value']);
+      e.target['value'] = this.numStr(
+        e.target['value'].replace(/\s/g, ''),
+        ' '
+      ).toString();
+    });
+  }
 
   round(float: number) {
     return Math.round(float * 100) / 100;
@@ -19,41 +27,39 @@ export class ImpotsComponent implements OnInit {
     let tranches_coeff = [0.11, 0.3, 0.41, 0.45];
     let tranches_impot = [0, 0, 0, 0, 0];
 
-    let amount_brut = document.getElementById('amount')['value'];
+    let amount_brut = Number(
+      document.getElementById('amount')['value'].replace(/\s/g, '')
+    );
+
+    console.log(amount_brut);
     let parts = document.getElementById('parts')['value'];
 
     let amount = amount_brut / parts;
 
-    if (amount > tranches[0]) {
-      //On atteind le max de non imposition
-      // tranches_impot[0] = tranches[0];
-      if (amount > tranches[1]) {
-        // On atteind le maximum imposable sur la première tranche d'imposition (tranches[1] - tranches[0])
-        tranches_impot[1] = (tranches[1] - tranches[0]) * tranches_coeff[0];
-        if (amount > tranches[2]) {
-          // On atteind le maximum imposable sur la deuxième tranche d'imposition (tranches[2] - tranches[1])
-          tranches_impot[2] = (tranches[2] - tranches[1]) * tranches_coeff[1];
-          if (amount > tranches[3]) {
-            // On atteind le maximum imposable sur la troisième tranche d'imposition (tranches[3] - tranches[2])
-            tranches_impot[3] = (tranches[3] - tranches[2]) * tranches_coeff[2];
+    // On atteind la deuxième tranche imposable donc on paie le max d'imposition sur la première
+    if (amount > tranches[1]) {
+      // On atteind le maximum imposable sur la première tranche d'imposition (tranches[1] - tranches[0])
+      tranches_impot[1] = (tranches[1] - tranches[0]) * tranches_coeff[0];
+      if (amount > tranches[2]) {
+        // On atteind le maximum imposable sur la deuxième tranche d'imposition (tranches[2] - tranches[1])
+        tranches_impot[2] = (tranches[2] - tranches[1]) * tranches_coeff[1];
+        if (amount > tranches[3]) {
+          // On atteind le maximum imposable sur la troisième tranche d'imposition (tranches[3] - tranches[2])
+          tranches_impot[3] = (tranches[3] - tranches[2]) * tranches_coeff[2];
 
-            // Et on attein la quatrième tranche dans laquelle il y a tout se qui dépasse
-            tranches_impot[4] = (amount - tranches[3]) * tranches_coeff[3];
-          } else {
-            //On atteind la troisième tranche mais pas la quatrième
-            tranches_impot[3] = (amount - tranches[2]) * tranches_coeff[2];
-          }
+          // Et on attein la quatrième tranche dans laquelle il y a tout se qui dépasse
+          tranches_impot[4] = (amount - tranches[3]) * tranches_coeff[3];
         } else {
-          //On atteind la deuxième tranche mais pas la troisième
-          tranches_impot[2] = (amount - tranches[1]) * tranches_coeff[1];
+          //On atteind la troisième tranche mais pas la quatrième
+          tranches_impot[3] = (amount - tranches[2]) * tranches_coeff[2];
         }
       } else {
-        //On atteind la première tranche d'imposition mais pas la seconde
-        tranches_impot[1] = (amount - tranches[0]) * tranches_coeff[0];
+        //On atteind la deuxième tranche mais pas la troisième
+        tranches_impot[2] = (amount - tranches[1]) * tranches_coeff[1];
       }
     } else {
-      //On n'attein pas la première tranche d'imposition
-      // tranches_impot[0] = amount;
+      //On attein pas la seconde tranche d'imposition, donc on pait ce qui dépasse de la première tranche au taux de la première
+      tranches_impot[1] = (amount - tranches[0]) * tranches_coeff[0];
     }
 
     //On calcule le total des
@@ -74,5 +80,21 @@ export class ImpotsComponent implements OnInit {
     document.getElementById(
       'total-amount'
     ).textContent = total_impot.toString();
+  }
+
+  //séapre par b les milliers du nombre a
+  numStr(a, b) {
+    a = '' + a;
+    b = b || ' ';
+    var c = '',
+      d = 0;
+    while (a.match(/^0[0-9]/)) {
+      a = a.substr(1);
+    }
+    for (var i = a.length - 1; i >= 0; i--) {
+      c = d != 0 && d % 3 == 0 ? a[i] + b + c : a[i] + c;
+      d++;
+    }
+    return c;
   }
 }
